ab3c24bb93ec41e0a6598a2ba76951df9741dce7
James Jackaman <james.jackaman@ntnu.no>
2024-02-08 13:42:49 +0100

    Davide's generalised advection function

--- data_generation/call_linadv.py
+++ data_generation/call_linadv.py
@@ -7,19 +7,28 @@ import argparse
 import pickle
 import random
 import os
-from firedrake import sin, cos, pi
+from firedrake import sin, cos, pi, exp, Constant
 
 #local
 import linadv
 
 #Generate a 'random' function
 def rand_func(x,y):
-    shift_x = random.random()
-    shift_y = random.random()
-    int1 = random.randint(5,8)
-    int2 = random.randint(5,8)
+    a = random.randint(1,10)
+    b = random.randint(1,10)
+    c = random.random()
+    e = random.random()
+    m1 = random.random()
+    m2 = random.random()
+    m3 = random.random()
+    m4 = random.random()
+
+    x_p = m1 * x + m2 * y
+    y_p = m3 * x + m4 * y
+    x_ = x_p
+    y_ = y_p
     
-    return sin(2 * pi * int1 * (x-shift_x)) * cos(2 * pi * int2 * (y-shift_y)) + 1
+    return exp(-(x_-e)**2-(y_-c)**2)*sin(2*pi*a*x_)*sin(2*pi*b*y_)*x*(1-x)*y*(1-y) / 0.04
 
 
 if __name__=="__main__":
